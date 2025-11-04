3ea8f39d05c32c176cac43ecf718f4c609169f4b
Md. Suman Sarkar <79307272+sumancse26@users.noreply.github.com>
2025-11-04 15:27:40 +0600

    add location list api display code

--- src/app/api/location/route.js
+++ src/app/api/location/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/config/prisma';
+
 export const GET = async (req) => {
     try {
         const location_list = await prisma.geo_Locations.findMany({
@@ -13,17 +14,39 @@ export const GET = async (req) => {
             }
         });
 
+        const processedLocations = location_list.map((loc) => {
+            let short_address = loc.full_address;
+
+            if (short_address && short_address.includes('>')) {
+                const parts = short_address.split('>').map((p) => p.trim());
+                const lastThree = parts.slice(-4).join(' > ');
+                short_address = lastThree;
+            }
+
+            return {
+                ...loc,
+                full_address: short_address
+            };
+        });
+
         return NextResponse.json(
             {
                 message: 'Location fetched successfully',
                 success: true,
-                total: location_list.length || 0,
-                location_list
+                total: processedLocations.length || 0,
+                location_list: processedLocations
             },
             { status: 200 }
         );
     } catch (error) {
-        console.error('Error parsing request body:', error);
-        return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+        console.error('Error fetching locations:', error);
+        return NextResponse.json(
+            {
+                success: false,
+                message: 'An error occurred while fetching locations.',
+                error: error.message
+            },
+            { status: 500 }
+        );
     }
 };
